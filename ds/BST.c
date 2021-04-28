b28a7eb5beef41a346f379f21bd4e4a7a6b3d962
Pavan-Parker <ppavanrockz@gmail.com>
2021-04-28 14:36:45 +0530

    --- Auto-Git Commit ---

--- ds/BST.c
+++ ds/BST.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 struct Node
 {
     int data;
@@ -8,29 +9,48 @@ struct Node
 
 void insertNode(struct Node* root,int data)
 {
-    struct Node *parent=root,*current;
-    struct Node *newNode=(struct Node*)(malloc(sizeof(struct Node)));    
+    struct Node* parent=root;
+    struct Node* current=root;
+    struct Node* newNode=(struct Node*)(malloc(sizeof(struct Node)));    
     newNode=&(struct Node){.data=data,.leftChild=NULL,.rightChild=NULL};
-    if()
-    while(!current==NULL)
+    int  direction;
+    if(current==NULL){current=newNode;}
+    else
     {
-        if(current->data>=data){current=current->leftChild;}        
-        else{current=current->rightChild;}        
+        while(!(current==NULL))
+        {
+            parent=current;
+            printf("%d",current->data);
+            if(current->data>=data){direction=0; current=current->leftChild;}        
+            else{direction=1;current=current->rightChild;}        
+        }
+        if(direction==0){parent->leftChild=newNode;}
+        else{parent->leftChild=newNode;}
     }
-    current=newNode;
 }
 void printInorder(struct Node* root)
 {
     struct Node* current=root;    
-    while (!current==NULL){}
+    while (!(current==NULL))
+    {
+        printInorder(current->leftChild);
+        printf("%d -> ",current->data);
+        printInorder(current->leftChild);
+    }
+    printf("\t done Printing\n");
 }
 int main()
 {
-    struct Node * root=NULL;
+    struct Node* root=NULL;
     insertNode(root,3);
+    printInorder(root);
     insertNode(root,1);
+    printInorder(root);
     insertNode(root,4);
+    printInorder(root);
     insertNode(root,9);
+    printInorder(root);
     insertNode(root,6);
+    printInorder(root);
     return 0;
 }
