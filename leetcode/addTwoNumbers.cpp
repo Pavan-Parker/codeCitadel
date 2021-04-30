d6b6d68995d39c408ac3b0ef2b6db451d68cb909
Pavan-Parker <ppavanrockz@gmail.com>
2021-04-30 20:02:58 +0530

    --- Auto-Git Commit ---

--- leetcode/addTwoNumbers.cpp
+++ leetcode/addTwoNumbers.cpp
@@ -43,12 +43,17 @@ public:
         int i=n;
         struct ListNode* root=new ListNode(0,NULL);
         struct ListNode* temp=root;
-        while(i)
+        while(1)
         {
             temp->val=i%10;
-            temp->next=new ListNode(0,NULL);
-            temp=temp->next;
             i=i/10;
+            if(!i)
+            {
+                temp->next=new ListNode(0,NULL);
+                temp=temp->next;
+            }
+            else{break;}
+
         }
         return root;
 
@@ -62,33 +67,39 @@ public:
         
     }
 };
-void printLL(ListNode* head)
-{
-    ListNode* temp=head;
-    while(temp!=NULL)
-    {
-        cout<<temp->val<< " -> ";
-        temp=temp->next;
-    }
-    cout<<endl;
-}
+// void printLL(ListNode* head)
+// {
+//     ListNode* temp=head;
+//     while(temp!=NULL)
+//     {
+//         cout<<temp->val<< " -> ";
+//         temp=temp->next;
+//     }
+//     cout<<endl;
+// }
 
 int main()
 {
-   struct ListNode* head=new ListNode();
-   printLL(head);
+//    struct ListNode* head=new ListNode();
+//    printLL(head);
 
-   struct ListNode* second=new ListNode(1);
-   head->next=second;
-   printLL(head);
+//    struct ListNode* second=new ListNode(1);
+//    head->next=second;
+//    printLL(head);
    
-   struct ListNode* fourth=new ListNode(3);
-   printLL(head);
+//    struct ListNode* fourth=new ListNode(3);
+//    printLL(head);
    
-   struct ListNode* third=new ListNode(2,fourth);
-   second->next=third;
-   printLL(head);
+//    struct ListNode* third=new ListNode(2,fourth);
+//    second->next=third;
+//    printLL(head);
+
+    Solution s;
+    struct ListNode* x=s.split();
+    struct ListNode* x=s.split();
+
 
 
    return 0;
+
 }
