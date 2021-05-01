b60d5725640c50ce9b1b0852c6081873f3c47f26
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-01 11:53:27 +0530

    --- Auto-Git Commit ---

--- leetcode/longestSubstringWOrepeat.cpp
+++ leetcode/longestSubstringWOrepeat.cpp
@@ -6,12 +6,18 @@ public:
         unordered_map<char,vector<int>> map;
         for(int i=0;i<s.length();i++)
         {
-            auto found=map.find(s[i]);
-            if(found!=map.end())
-            {
-                if((found->second[1]-found->second[0])>(i-found->second[1]) ){}
-            }
+            // auto found=map.find(s[i]);
+            // if(found!=map.end())
+            // {
+            //     if((found->second[1]-found->second[0])>(i-found->second[1]) ){}
+            // }
+            map[s[i]].push_back(i);
         }
-        
+        for(auto it:map)
+        {
+            cout<<it.first<<" : ";
+            copy(it.second.begin(),it.second.end(),ostream_iterator<char>(cout," "));
+            cout<<endl;
+        }
     }
 };
