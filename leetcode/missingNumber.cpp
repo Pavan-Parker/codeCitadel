6f8409666669a056f067c9f318144c8115b8da3f
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-03 13:28:11 +0530

    --- Auto-Git Commit ---

--- leetcode/missingNumber.cpp
+++ leetcode/missingNumber.cpp
@@ -20,4 +20,33 @@ public:
         for(int i=1;i<nums.size();i++){temp^=nums[i];}
         return (des^temp)%(int)pow(2,(int)log2(des));
     }
+};class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int temp=nums[0],n=nums.size();
+        int des,limit;
+        switch (n%4)
+        {
+        case 0:
+            des=n;
+            break;
+        case 1:
+            des=1;
+            break;
+        case 2:
+            des=n+1;
+            break;
+        case 3:
+            des=0;
+            break;
+        }
+        
+        for(int i=1;i<nums.size();i++){temp^=nums[i];}
+        
+        cout<<"des^temp:"<<(des^temp)<<","<<"des:"<<des<<","<<"n:"<<n<<", "<<"limit:";
+        if(1){limit=(int)pow(2,(int)(log2(n)+1));cout<<limit<<endl;}
+        else{cout<<0<<endl;return 0;}
+        
+        return (des^temp)%limit;
+    }
 };
---
+++ leetcode/reverseBits.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    uint32_t maskAndSwap(uint32_t x,int size)
+    {
+        
+    }
+    uint32_t reverseBits(uint32_t n)
+    {
+        
+    }
+};
