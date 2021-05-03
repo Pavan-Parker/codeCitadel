c2dc9ede8a946b858021b8f9395f5c54b27cbc0c
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-03 14:28:23 +0530

    --- Auto-Git Commit ---

--- leetcode/reverseBits.cpp
+++ leetcode/reverseBits.cpp
@@ -2,17 +2,14 @@
 using namespace std;
 class Solution {
 public:
-    uint32_t maskAndSwap(uint32_t x,int lower,int higher)
-    {
-        
-        return (x>>(size/2)) | x<<(size/2); 
-    }   
     uint32_t reverseBits(uint32_t n)
     {
-        int size;
-        while(1)
+        uint32_t rev,allOnes=0xFFFFFFFF;
+        for(int i=0;i<31;i++)
         {
-
+            if(n&1){rev=(rev>>1)|allOnes;}
+            else{rev>>=1;}
         }
+
     }
 };
