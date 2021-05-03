186588f24c9738305fdeb73a113dfd92fbca74e7
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-03 15:58:43 +0530

    --- Auto-Git Commit ---

--- leetcode/reverseBits.cpp
+++ leetcode/reverseBits.cpp
@@ -4,11 +4,13 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n)
     {
-        uint32_t rev,allOnes=0xFFFFFFFF;
-        for(int i=0;i<31;i++)
+        uint32_t rev;
+        for(int i=0;i<32;i++)
         {
-            if(n&1){rev=(rev>>1)|allOnes;}
-            else{rev>>=1;}
+            cout<<(n&1)<<" ";
+            if(n&1){rev=(rev<<1)|1;}
+            else{rev<<=1;}
+            n>>=1;
         }
 
         return rev;
