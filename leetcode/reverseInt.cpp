306283284bbeb75e08b969bf1a219ad0a71a4897
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-06 23:59:53 +0530

    --- Auto-Git Commit ---

---
+++ leetcode/reverseInt.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// class Solution {
+// public:
+//     int reverse(int x) {
+        
+//     }
+// };
+
+int main()
+{
+    int s;
+    cin>>s;
+    string inStr=to_string(s),outStr;
+    cout<<inStr[0]<<endl;
+    
+    return 0;
+}
