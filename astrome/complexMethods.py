25e13c2e6238268b0ee429b7e1e2f4b12e978e6d
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-21 12:56:11 +0530

    --- Auto-Git Commit ---

--- astrome/complexMethods.py
+++ astrome/complexMethods.py
@@ -2,18 +2,18 @@ import math
 from typing_extensions import ParamSpecArgs
 class Complex(object):
     def __init__(self, real, imaginary):
-        pass
+        self.real=real
+        self.imaginary=imaginary
     def __add__(self, no):
-        return Complex(self.real+no.real,self.imaginary+no.imaginary)
-        pass
+        return Complex(self.real+no.real,   self.imaginary+no.imaginary)
     def __sub__(self, no):
-        pass
+        return Complex(self.real-no.real,   self.imaginary-no.imaginary)
     def __mul__(self, no):
-        pass
+        return Complex(self.real*no.real-self.imaginary*no.imaginary,   self.real*no.imaginary+self.imaginary*no.real)
     def __truediv__(self, no):
-        pass
+        return Complex(self.real*no.real+self.imaginary*no.imaginary,   -self.real*no.imaginary+self.imaginary*no.real)
     def mod(self):
-        pass
+        return Complex(math.sqrt(pow(self.real,2)+pow(self.imaginary,2)),0)
 
      
     def __str__(self):
