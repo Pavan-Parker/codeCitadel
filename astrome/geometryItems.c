5d83ef336381ee8b7ebd96738c5450fd3e0c2916
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-21 20:57:52 +0530

    --- Auto-Git Commit ---

--- astrome/geometryItems.c
+++ astrome/geometryItems.c
@@ -105,7 +105,7 @@ int main()
   scanf("%d", &n);
   printf("%d geometric items\n", n);
 
-  object = (struct Geometry **)malloc(sizeof(struct Geometry*)*n);
+  object = malloc(sizeof(struct Geometry*)*n);
 
   for(int i = 0; i < n; i++)
   {
---
+++ astrome/waterLevel.py
@@ -0,0 +1,4 @@
+import numpy as np 
+a=np.zeros()
+for i in range(len(int(input()))):
+    
