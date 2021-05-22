e7eb6946774032671edd659b518c0013d06af7ec
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-22 08:26:36 +0530

    --- Auto-Git Commit ---

--- astrome/waterLevel.py
+++ astrome/waterLevel.py
@@ -1,4 +1,12 @@
-import numpy as np 
-a=np.zeros()
-for i in range(len(int(input()))):
+import numpy as np
+
+matrixSize=int(input())
+arr=np.zeros((matrixSize,matrixSize))
+
+for i in range(matrixSize):
+    arr[i]=list(map( int ,input().split()))
+
+currentCell=[int(matrixSize/2),int(matrixSize/2)]
+while(True):
     
+    arr[currentCell]
