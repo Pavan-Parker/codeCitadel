662ff059d3998ad403349e12ef2b67dccc99d37c
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-22 10:57:08 +0530

    --- Auto-Git Commit ---

--- astrome/waterLevel.py
+++ astrome/waterLevel.py
@@ -1,18 +1,15 @@
-import numpy as np
-from numpy.core.fromnumeric import argmin
-
 def getMinNeighbour(arr,currentCell):
     try:
-        arr=np.array(arr)
         x,y=currentCell
-        index=np.array([arr[x-1][y],arr[x+1][y],arr[x][y-1],arr[x][y+1]]).argmin()
+        neighbours=[arr[x-1][y],arr[x+1][y],arr[x][y-1],arr[x][y+1]]
+        index=neighbours.index(min(neighbours))
         if   index== 0: return [x-1,y]
         elif index== 1: return [x+1,y]
         elif index== 2: return [x,y-1]
         elif index== 3: return [x,y+1]
     except IndexError as error:
         print("hault")
-        return np.NaN
+        return None
 def main():
     matrixSize=int(input())
     arr=np.zeros((matrixSize,matrixSize))
@@ -22,6 +19,10 @@ def main():
     currentCell=[int(matrixSize/2),int(matrixSize/2)]
     while(True):
         out[tuple(currentCell)]='W'
-        ifgetMinNeighbour(arr,currentCell)
+        ret=getMinNeighbour(arr,currentCell)
+        if(not np.isnan(ret).all()):
+            currentCell=ret
+        else:break
+    print(out)
 if __name__ == "__main__":
     main()
