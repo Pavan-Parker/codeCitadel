1539291ddb0aa5562f38e2e8e66b8e737b8674e2
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-22 11:57:21 +0530

    --- Auto-Git Commit ---

--- astrome/waterLevel.py
+++ astrome/waterLevel.py
@@ -1,12 +1,17 @@
-def getMinNeighbour(arr,currentCell):
+import snoop
+import loguru
+@loguru.logger.catch()
+def getMinNeighbour(arr,currentCell,coveredCells):
     try:
         x,y=currentCell
-        neighbours=[arr[x-1][y],arr[x+1][y],arr[x][y-1],arr[x][y+1]]
-        index=neighbours.index(min(neighbours))
-        if   index== 0: return [x-1,y]
-        elif index== 1: return [x+1,y]
-        elif index== 2: return [x,y-1]
-        elif index== 3: return [x,y+1]
+        neighbours=[[x-1,y],[x+1,y],[x,y-1],[x,y+1]]
+        # index=neighbours.index(min(neighbours))
+        minIndex=[-1,-1]
+        for index in neighbours:
+            if((arr[index[0]][index[1]]<=arr[minIndex[0]][minIndex[1]]) and (index not in coveredCells)):
+                minIndex=index
+        return minIndex
+
     except IndexError as error:
         print("hault")
         return None
@@ -15,6 +20,8 @@ def makeTile(value,size):
     row=[value]*size
     for i in range(size): retMat.append(row)
     return retMat
+
+# @snoop
 def main():
     matrixSize=int(input())
     arr=makeTile(0,matrixSize)
@@ -22,11 +29,15 @@ def main():
 
     for i in range(matrixSize):
         arr[i]=list(map( int ,input().split()))
+
     currentCell=[int(matrixSize/2),int(matrixSize/2)]
+    coveredCells=[]
     while(True):
+        print(out)
         out[currentCell[0]][currentCell[1]]='W'
-        ret=getMinNeighbour(arr,currentCell)
+        ret=getMinNeighbour(arr,currentCell,coveredCells)
         if(ret is not None):
+            coveredCells.append(currentCell)
             currentCell=ret
         else:break
     print(out)
