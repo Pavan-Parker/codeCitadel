543545335c936d5196873ea50dc4a99d11a141f0
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-22 19:59:05 +0530

    --- Auto-Git Commit ---

--- astrome/waterLevel.py
+++ astrome/waterLevel.py
@@ -9,16 +9,16 @@ def getMinNeighbour(arr,waterLevel,coveredCells):
         for index in neighbours:
             if((index not in coveredCells)):
                 neighboursAll[index]=arr[index[0]][index[1]]
-                if(("minIndex" not in locals()) or (arr[index[0]][index[1]]<=arr[minIndex[0]][minIndex[1]])):
-                    minIndex=index
 
-    
     neighboursAllSorted = {k: v for k, v in sorted(neighboursAll.items(), key=lambda x: x[1])}
 
     for k,v in neighboursAll.items():
         if(v<=waterLevel): minIndices.append(k)
         else:break
-    return minIndices
+    if(len(minIndices)==0):
+        return [False,[neighboursAllSorted[0][0]]]
+    else:
+        return [True,minIndices]
 def makeTile(value,size):
     out=[]
     for i in range(size):
@@ -49,13 +49,20 @@ def main():
     waterLevel=arr[coveredCells[0][0]][coveredCells[0][1]]
     while(True):
         printMatrix(out)
-        ret=getMinNeighbour(arr,waterLevel,coveredCells)      
-        out[ret[0]][ret[1]]='W'
-        newLevel=arr[ret[0]][ret[1]]
-        if((not (ret[0] in [0,matrixSize-1])) and (not (ret[1] in [0,matrixSize-1])) ):
-            if(waterLevel<newLevel): waterLevel=newLevel
-            coveredCells.append(ret)
-        else:break
+        retList=getMinNeighbour(arr,waterLevel,coveredCells)      
+        if(retList[0]):
+            for ret in retList[1]:
+                out[ret[0]][ret[1]]='W'
+                if((not (ret[0] in [0,matrixSize-1])) and (not (ret[1] in [0,matrixSize-1])) ):
+                    coveredCells.append(ret)
+                else:break
+        else:
+            ret=retList[1][0]
+            out[ret[0]][ret[1]]='W'
+            if((not (ret[0] in [0,matrixSize-1])) and (not (ret[1] in [0,matrixSize-1])) ):
+                    coveredCells.append(ret)
+            else:break
+            waterLevel=arr[ret[0]][ret[1]]
 
     printMatrix(out)
 
