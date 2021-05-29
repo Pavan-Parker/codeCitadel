e7d2aa870c8d7820aacc4f035c4a6bbc84011e55
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-29 08:59:27 +0530

    --- Auto-Git Commit ---

--- cpp/classes/class.cpp
+++ cpp/classes/class.cpp
@@ -4,18 +4,34 @@ using namespace std;
 class hmm
 {
     public: //Access modifier
+
         int var;
+
         hmm()   //default constructor
         {
             cout << "default constructor"<< endl;
-            var=1;
+            hmm::var=1;
         }
+
         hmm(int var)    //parameterized constructor
         {
             hmm::var=var;
         }
+
+        hmm(hmm &hmmGiven)     //Copt constructor  
+        {
+            hmm::var=hmmGiven.var;
+        }
+
+        ~hmm()
+        {
+            cout<<"destructor for hmm is called"<<endl;
+        }
+
         void printVar();
 };
+
+class hmmChild:hmm{}
 
 void hmm::printVar()    // out of class defination
 {
@@ -26,8 +42,13 @@ int main()
 {
     hmm obj1;
     obj1.printVar();
+
     hmm obj2(2);
     obj2.printVar();
+    
+    hmm obj1Copy=obj1;
+    obj1Copy.printVar();
+
     return 0;
 
 }
