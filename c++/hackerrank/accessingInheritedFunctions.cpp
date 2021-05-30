2edd5ceaf4f098c6566ed358e267a0828e3f4bc3
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-30 22:57:04 +0530

    --- Auto-Git Commit ---

---
+++ c++/hackerrank/accessingInheritedFunctions.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+    public:
+        A(){
+            callA = 0;
+        }
+    private:
+        int callA;
+        void inc(){
+            callA++;
+        }
+
+    protected:
+        void func(int & a)
+        {
+            a = a * 2;
+            inc();
+        }
+    public:
+        int getA(){
+            return callA;
+        }
+};
+
+class B
+{
+    public:
+        B(){
+            callB = 0;
+        }
+    private:
+        int callB;
+        void inc(){
+            callB++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 3;
+            inc();
+        }
+    public:
+        int getB(){
+            return callB;
+        }
+};
+
+class C
+{
+    public:
+        C(){
+            callC = 0;
+        }
+    private:
+        int callC;
+        void inc(){
+            callC++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 5;
+            inc();
+        }
+    public:
+        int getC(){
+            return callC;
+        }
+};
+
+class D : public A, public B, public C
+{
+
+	int val;
+	public:
+		//Initially val is 1
+		 D()
+		 {
+		 	val = 1;
+		 }
+
+
+		 //Implement this function
+		 void update_val(int new_val)
+		 {
+            int values[3]={2,3,5};
+            
+            for(int i=0,temp;i<3;i++)
+            {
+                while (!(new_val%values[i]))
+                {
+                    if(i==0)
+                        A::func(val);  
+                    else if (i==1)
+                        B::func(val);  
+                    else
+                        C::func(val);  
+
+                    new_val/=values[i];
+                }
+                
+            }
+		 }
+		 //For Checking Purpose
+		 void check(int); //Do not delete this line.
+};
+
+
+
+void D::check(int new_val)
+{
+    update_val(new_val);
+    cout << "Value = " << val << endl << "A's func called " << getA() << " times " << endl << "B's func called " << getB() << " times" << endl << "C's func called " << getC() << " times" << endl;
+}
+
+
+int main()
+{
+    D d;
+    int new_val;
+    cin >> new_val;
+    d.check(new_val);
+
+}
