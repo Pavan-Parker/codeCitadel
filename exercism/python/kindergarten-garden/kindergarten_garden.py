95fce2c3637676095844720fa0ccb65bf0adba3c
Pavan-Parker <ppavanrockz@gmail.com>
2021-05-30 23:57:17 +0530

    --- Auto-Git Commit ---

--- exercism/python/kindergarten-garden/kindergarten_garden.py
+++ exercism/python/kindergarten-garden/kindergarten_garden.py
@@ -1,10 +1,13 @@
 class Garden:
-    def __init__(self, diagram, students):
+    expand={'G':"Grass",'R':"Radishes",'C':"Clover","V":"Violets"}
+    def __init__(self, diagram, students=["Alice", "Bob", "Charlie", "David","Eve", "Fred", "Ginny", "Harriet","Ileana", "Joseph","Kincaid", "Larry"]):
         self.diagram=diagram
         self.students=students
 
     def plants(self,student):
-        if student in self.students:
-            
-        else:
-            
+        try:
+            offset=(len(self.diagram)+1)/2
+            studentID=(self.students).index(student)
+            return list(map(lambda x :  self.expand[self.diagram[x]],[studentID*2,(studentID*2)+1,offset+(studentID*2),offset+(studentID*2)+1,]))
+        except ValueError as e:
+            raise Exception("student not found")
