afbb5c0625fb423cf168fb8123be7c3194017359
Pavan-Parker <ppavanrockz@gmail.com>
2021-06-29 22:29:36 +0530

    kinderGarden

--- exercism/python/kindergarten-garden/kindergarten_garden.py
+++ exercism/python/kindergarten-garden/kindergarten_garden.py
@@ -2,11 +2,11 @@ class Garden:
     expand={'G':"Grass",'R':"Radishes",'C':"Clover","V":"Violets"}
     def __init__(self, diagram, students=["Alice", "Bob", "Charlie", "David","Eve", "Fred", "Ginny", "Harriet","Ileana", "Joseph","Kincaid", "Larry"]):
         self.diagram=diagram
-        self.students=students
+        self.students=sorted(students)
 
     def plants(self,student):
         try:
-            offset=(len(self.diagram)+1)/2
+            offset=int((len(self.diagram)+1)/2)
             studentID=(self.students).index(student)
             return list(map(lambda x :  self.expand[self.diagram[x]],[studentID*2,(studentID*2)+1,offset+(studentID*2),offset+(studentID*2)+1,]))
         except ValueError as e:
