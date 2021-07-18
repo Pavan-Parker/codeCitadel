f09de8635e151729114a73af634e870d48cb6aca
Pavan Kumar Pothula <31162938+Pavan-Parker@users.noreply.github.com>
2021-07-18 22:30:25 +0530

    working but not perfect

--- SystemVerilog/fifo.sv
+++ SystemVerilog/fifo.sv
@@ -1,4 +1,31 @@
+/*Basic FIFO designed for AXI4-Stream*/
 
+/*
+*     
+*    WORKING:                    
+*                ----------->[S----FIFO-----M]-------------
+*                |                                         |
+*        (Write  |                                         | (Read
+*        to      |                                         | from
+*        FIFO)   |                                         | FIFO)
+*                |                                         |
+*                -------------[M---DRIVER---S]--------------
+*                                DRIVER
+*
+*        - To Write into FIFO,
+*            Driver will use it's MASTER ports (M_*) to send the data and
+*            FIFO will use it's SLAVE ports (S_*) to receive the data
+*        - To Read from FIFO,
+*            FIFO will use it's MASTER ports (M_*) to send the data and
+*            Driver will use it's SLAVE ports (S_*) to receive the data
+*        
+*        - To avoid race condition,
+*            "Write into FIFO" operation is prioritized over "Read from FIFO" operation,
+*            Which means "Read from FIFO" will be operational only when there's no data "Writing into FIFO" 
+*    NOTE: 
+*        In the current state, there is no packing of the data,
+*        So, TLast signal is not used as it is not relavant anymore. 
+*/
 module fifo #
 (
     parameter depth      = 8,  //FIFO depth
@@ -6,20 +33,20 @@ module fifo #
 )
 (
 /*AXI-Stream Interfaces*/
-    
+
     input logic  CLK,        // CLOCK SIGNAL
 
-    /*AXIS INPUT FIFO*/
+    /*AXIS WRITE INTO FIFO*/
     input logic  [width-1:0] S_TData,    // Transmission Data
     input logic              S_TValid,   // Master ready
-    input logic              S_TLast,    // Last byte of packet
+    input logic              S_TLast,    // Last byte of packet, NOT USED AS OF NOW
     output logic             S_TReady,   // Slave ready
 
-    /*AXIS OUTPUT FIFO*/
-    output logic  [width-1:0] M_TData,    // Transmission Data
-    output logic            M_TValid,   // Master ready
-    output logic            M_TLast,    // Last byte of packet
-    input  logic            M_TReady,   // Slave ready
+    /*AXIS READ FROM FIFO*/
+    output logic  [width-1:0] M_TData,   // Transmission Data
+    output logic            M_TValid,    // Master ready
+    output logic            M_TLast,     // Last byte of packet, NOT USED AS OF NOW
+    input  logic            M_TReady,    // Slave ready
 
 /*Signals for Debugging purposes*/
 
@@ -31,58 +58,52 @@ module fifo #
     input Reset
 );
 
-    // logic receptionComplete=0;
-
-    /*
-    *                           FIFO
-                ----------->[S------------M]---------------------
-                |                                                 |
-                |                                                 |
-                |                                                 |
-                -------------[M------------S]---------------------
-                                DRIVER
-    */
-
+    /*Assert WriteInto_Ready signal if Writer is ready and Buffer isn't full*/
     assign S_TReady=( S_TValid && !isFull);
 
+    /*Assert ReadFrom_Ready signal if there's no writing Operation going on and Buffer isn't empty*/
     assign M_TValid=(!S_TReady && !isEmpty);
 
-  logic [0:(width-1)] memory [0:(depth-1)];
-  logic [0:($clog2(depth)-1)] readPointer='0;
-  logic [0:($clog2(depth)-1)] writePointer='0;
-  logic [0:($clog2(depth))] count='0;
+    /*Storage of (Depth)*(Width)*/
+    logic [0:(width-1)] memory [0:(depth-1)];
 
+    /*Pointers for tracking*/    
+    logic [0:($clog2(depth)-1)] readPointer='0;
+    logic [0:($clog2(depth)-1)] writePointer='0;
+    logic [0:($clog2(depth))] count='0;
+
+    /*Flags for underflow and Overflow*/    
     assign isEmpty=(!count);
     assign isFull=(count==depth);
 
-//     always_ff @(posedge CLK) begin : MONITOR
-//         $display("@CLK=0x%0t",$time/10);
-//         $display($sformatf("%p", memory));
-//     end : MONITOR
-
-    initial begin
-        $dumpfile("tb_fifo.vcd");
-        $dumpvars(0, tb_fifo);
-    end
     always_ff @(posedge CLK) begin
+
+            /*Reset the FIFO*/
             if(Reset) begin : RESET
                 writePointer<='0;
                 readPointer<='0;
                 count<='0;
             end : RESET
         
+            /*Reception*/
             else if(S_TReady) begin : Reception
-                memory[writePointer]<=S_TData;
-                writePointer<=(writePointer+1);
-                count<=count+1'b1;
+                memory[writePointer]<=S_TData;      //Write to storage
+                writePointer<=(writePointer+1);     //Increment the write pointer
+                count<=count+1'b1;                  //Increment the counter
             end : Reception
     
+            /*Sender*/
             else if(M_TValid) begin  : Sending
-                M_TData<=memory[readPointer];
-                readPointer<=(readPointer+1);
-                count<=count-1'b1;
+                M_TData<=memory[readPointer];       //Read from storage
+                readPointer<=(readPointer+1);       //Increment the read pointer
+                count<=count-1'b1;                  //Decrement the counter
             end : Sending
+    end
 
+    /*VCD dump for debugging*/
+    initial begin
+        $dumpfile("tb_fifo.vcd");
+        $dumpvars(0, tb_fifo);
     end
 
 endmodule
--- SystemVerilog/testFifo.sv
+++ SystemVerilog/testFifo.sv
@@ -1,21 +1,31 @@
-`timescale 1ns/1ps
+/*Testbench for testing FIFO designed for AXI4-Stream*/
+/*
+    Working:
+        This is a simple testbench, where on reset, variable temp will be initialized to 'DEPTH', and
+            Mode 1 - driver will keep on writing temp and decrementing the temp, until FIFO says it's full (isFull) , then  
+            Mode 0 - driver will keep on reading FIFO, until FIFO says it's empty (isEmpty)
+        The above two cycles will keep repeating endlessly, but the simulaiton stops after a fixed time. 
+*/
 
+
+
+`timescale 1ns/1ps
 module tb_fifo ();
     
     parameter DEPTH=8,
-          WIDTH=8;
+              WIDTH=8;
 
 /*AXI-Stream Interfaces*/
     
     logic  CLK;        // CLOCK SIGNAL
 
-    /*Slave ports of tb*/
+    /*Slave ports of driver*/
     logic [0:WIDTH-1] M_TData;    // Transmission Data
     logic             M_TValid;   // Master ready
     logic             M_TLast;    // Last byte of packet
     logic             M_TReady;   // Slave ready
 
-    /*Master ports of tb*/
+    /*Master ports of driver*/
     logic [0:WIDTH-1] S_TData;    // Transmission Data
     logic             S_TValid;   // Master ready
     logic             S_TLast;    // Last byte of packet
@@ -29,8 +39,15 @@ module tb_fifo ();
 /*Reset and Flush the fifo*/
     
     logic Reset;
-  logic [0:$clog2(DEPTH)-1]temp=DEPTH;
-    logic START=0;
+
+/*temp variable (Data to write)*/
+    logic [0:$clog2(DEPTH)-1]temp=DEPTH;
+
+/*Mode:
+        1 = Driver writes to Buffer
+        0 = Driver reads from Buffer
+*/
+    logic mode=1;
 
 
 fifo 
@@ -41,70 +58,104 @@ fifo
 test_fifo
 (
 
+/*Connecting Clock Signal*/
     .CLK(CLK),
-    
+
+/*Connecting "WriteToFIFO" ports*/
     .S_TData(M_TData),
     .S_TValid(M_TValid),
     .S_TLast(M_TLast),
     .S_TReady(M_TReady),
 
-    
+/*Connecting "ReadFromFIFO" ports*/    
     .M_TData(S_TData),
     .M_TValid(S_TValid),
     .M_TLast(S_TLast),
     .M_TReady(S_TReady),
 
-
+/*Connecting Flag Signals*/
     .isEmpty(isEmpty),  // check if buffer is Empty
     .isFull(isFull),   // check if buffer is full
 
-/*Reset and Flush the fifo*/
-    
+/*Connecting Reset*/
     .Reset(Reset)
     
 );
 
 
-localparam CLK_PERIOD = 10;
+localparam CLK_PERIOD = 10; //setting clock period
 
+/*CLOCK GENERATOR*/
 always begin : clockGenerator
     #(CLK_PERIOD/2) CLK=~CLK;
 end
 
+/*INITIALIZATION*/
+initial begin
+
+    CLK<=0;                 //Clock singal initialize to 0
+    Reset<=1;               //Asset Reset FIFO on start
+    #(CLK_PERIOD) Reset<=0; //Release after a one clock period
+
+end
+
+
+
+/*Asserting Handshake singals depending on the mode*/
+always_ff @(mode) begin
+
+    if(mode) begin : WriteHandshake
+        M_TValid<=1;
+        S_TReady<=0;
+    end : WriteHandshake
+
+    else begin : ReadHandshake
+        M_TValid<=0;
+        S_TReady<=1;
+    end : ReadHandshake
+
+end
+
+always_ff @(posedge CLK) begin
+
+    /*if FIFO is full, switch to read mode*/
+  	if(isFull) begin : WriteCompletionCheck
+        mode<=0;
+    end : WriteCompletionCheck
+
+    /*if FIFO is empty, switch to write mode*/	
+  	if(isEmpty) begin : ReadCompletionCheck
+        mode<=1;
+    end : ReadCompletionCheck
+
+  	if(!Reset) begin
+    
+      /*Mode1=Writing temp and decrementing temp*/
+      if(mode) begin : SENDING
+          M_TData<=temp;
+          temp<=temp-1'b1;
+      end : SENDING
+
+      /*Mode0=Reading temp*/      
+      else begin : RECEIVING
+          temp<=S_TData;
+      end : RECEIVING
+      
+    end
+    
+end
+
+// Terminate simulation after a fixed time
+initial begin : TerminateSimulation
+    #300 $finish();
+end : TerminateSimulation
+
+
+    /*VCD dump for debugging*/
 // initial begin
 //     $dumpfile("tb_fifo.vcd");
 //     $dumpvars(0, tb_fifo);
 // end
-
-initial begin
-    CLK<=0;
-    Reset<=1;
-    M_TValid<=1;
-//     M_TData<=temp;
-    #(CLK_PERIOD) Reset<=0;
-    // START<=1;
-end
-//  initial begin
-//    #100 $finish();
-//  end
-
-always_ff @(posedge CLK) begin
-
-    if(isFull) begin : CompletionCheck
-        $finish;
-    end : CompletionCheck
-
-  	if(!Reset) begin : SENDING
-        M_TData<=temp;
-        temp<=temp-1'b1;
-    end : SENDING
-    
-    // if(!Reset&&temp) begin : RECEPTION
-    //     M_TData<=temp;
-    //     temp<=temp-1;
-    // end : RECEPTION
-    
-end
 
 
 
