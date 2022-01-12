d14a51b3f0e2c64f0c4fceab40bd5f84be98455c
Pavan_Parker <ppavanrockz@gmail.com>
2022-01-12 22:11:56 +0530

    January 12, 2022 10:11 PM

---
+++ codes/leetcode/product-of-array-except-self.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        vector<int> lr = {1};
+        vector<int> rl = {1};
+        
+        for (const int itr : nums)
+        {
+            lr.push_back(itr*lr.back());
+        }
+    }
+};
