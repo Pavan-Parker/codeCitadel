772031b832fe479b18b79ac7e3ef80622880e58c
Pavan_Parker <ppavanrockz@gmail.com>
2022-01-12 22:53:01 +0530

    January 12, 2022 10:53 PM

--- codes/leetcode/product-of-array-except-self.cpp
+++ codes/leetcode/product-of-array-except-self.cpp
@@ -9,19 +9,23 @@ public:
     {
         vector<int> lr = {1};
         vector<int> rl = {1};
+        int temp = 1;
         vector<int> output;
-        for (auto itr = nums.begin(); itr != nums.end(); itr++)
+        for ( auto itr = nums.begin(); itr != nums.end(); itr++)
         {
-            lr.insert(lr.end(), (*itr) * lr.back());
+            temp = temp * (*itr);
+            lr.insert(lr.end(), temp);
         }
         lr.erase(lr.end()-1);
-
-        for (auto itr = nums.rbegin(); itr != nums.rend(); itr++)
+        
+        temp = 1 ;
+        for ( auto itr = nums.rbegin(); itr != nums.rend(); itr++)
         {
-            rl.insert(rl.begin(), (*itr) * (rl.front()));
+            temp = temp * (*itr); 
+            rl.insert(rl.begin(), temp);
         }
         rl.erase(rl.begin());
-
+        
         for (size_t i = 0; i < lr.size(); i++)
         {
             output.push_back(lr.at(i) * rl.at(i));
