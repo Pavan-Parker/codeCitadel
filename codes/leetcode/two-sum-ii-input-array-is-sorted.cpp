2fafba37c90ae79591f68f93de0b51b41f09c6b0
Pavan_Parker <ppavanrockz@gmail.com>
2022-01-12 18:34:14 +0530

    January 12, 2022 6:34 PM

--- codes/leetcode/two-sum-ii-input-array-is-sorted.cpp
+++ codes/leetcode/two-sum-ii-input-array-is-sorted.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
-
+#include <unordered_map>
 using namespace std;
 
 class Solution
@@ -9,11 +9,19 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &numbers, int target)
     {
-        vector<int>::iterator i = numbers.begin();
-        vector<int>::iterator j = numbers.begin();
-        while(1)
+        unordered_map<int, int> lookup;
+
+        for (auto it = numbers.begin(); it < numbers.end(); it++)
         {
 
+            if (lookup.find(*it) == lookup.end())
+            {
+                lookup[target - *it] = it - numbers.begin();
+            }
+            else
+            {
+                return {lookup.at(*it), (int)(it - numbers.begin())};
+            }
         }
     }
 };
