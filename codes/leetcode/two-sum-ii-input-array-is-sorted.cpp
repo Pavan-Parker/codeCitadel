aa5969ba4fd669b005a49ac750103eb118dcfc3e
Pavan_Parker <ppavanrockz@gmail.com>
2022-01-12 17:57:45 +0530

    January 12, 2022 5:57 PM

---
+++ codes/leetcode/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        auto i = numbers.begin();
+    }
+};
