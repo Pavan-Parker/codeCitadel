f7f4370462cb582e4ae3b21c8db9f2bfdf83ed10
Pavan_Parker <ppavanrockz@gmail.com>
2022-01-21 12:51:57 +0530

    January 21, 2022 12:51 PM

---
+++ codes/leetcode/20.valid-parentheses.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ */
+/*
+ *====Psuedo Code====
+ * 1. Stack using Linked List
+ * 1a. Initialize a LL to null.
+ * 1b. push = add node to the start i.e. newNode->val = val; newNode->next= head; head = newNode;
+ * 1c. pop = return head->val; head = head->next;
+ * 2. push incoming opening parenthesis.
+ * 3. pop if the incoming closing parenthesis matches the top else return false.
+ * 4. Repeat while not EOL.
+ */
+// @lc code=start
+class Node
+{
+public:
+};
+class LLStack
+{
+public
+    push()
+};
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+    }
+};
+// @lc code=end
