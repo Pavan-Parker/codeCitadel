ece0e3cc0eaead8b830f1d2f8131297b2da44679
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-17 11:41:55 +0530

    February 17, 2022 11:41 AM

--- codes/c/pointers.c
+++ codes/c/pointers.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 #define linebreak() printf("+++++++++++++++++++++++++++++\n")
 
-void increment(int*  x)
+void increment(int *x)
 {
     (*x)++;
 }
@@ -10,25 +10,24 @@ int main()
 {
     linebreak();
 
-// ! typecasting pointers  ! //
-    
-    int a=1025; //0x0041
-    int *ptr=&a;
-    char *ptrChar=(char *)&a;
-    
+    //typecasting pointers
+
+    int a = 1025; // 0x0041
+    int *ptr = &a;
+    char *ptrChar = (char *)&a;
+
     // ? ptrChar stepsize is 1 Byte. ? //
-    printf("ptr at %d is %d\n",ptr,*ptr);
-    printf("ptrChar+0 at %d is %d\n",ptrChar+0,*ptrChar);
-    printf("ptrChar+1 at %d is %d\n",ptrChar+1,*(ptrChar+1));
-    printf("ptrChar+2 at %d is %d\n",ptrChar+2,*(ptrChar+2));
-    printf("ptrChar+3 at %d is %d\n",ptrChar+3,*(ptrChar+3));
+    printf("ptr at %d is %d\n", ptr, *ptr);
+    printf("ptrChar+0 at %d is %d\n", ptrChar + 0, *ptrChar);
+    printf("ptrChar+1 at %d is %d\n", ptrChar + 1, *(ptrChar + 1));
+    printf("ptrChar+2 at %d is %d\n", ptrChar + 2, *(ptrChar + 2));
+    printf("ptrChar+3 at %d is %d\n", ptrChar + 3, *(ptrChar + 3));
     linebreak();
 
-// ! ptr as function arguments. ! //
+    // ! ptr as function arguments. ! //
 
     increment(ptr);
-    printf("ptr at %d is %d\n",ptr,*ptr);
-
+    printf("ptr at %d is %d\n", ptr, *ptr);
 
     return 0;
 }
