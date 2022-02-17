f8a4d87f100ada3f61792141dbdff1a7c08b6fde
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-17 11:42:50 +0530

    February 17, 2022 11:42 AM

--- codes/c/pointers.c
+++ codes/c/pointers.c
@@ -17,11 +17,11 @@ int main()
     char *ptrChar = (char *)&a;
 
     // ? ptrChar stepsize is 1 Byte. ? //
-    printf("ptr at %d is %d\n", ptr, *ptr);
-    printf("ptrChar+0 at %d is %d\n", ptrChar + 0, *ptrChar);
-    printf("ptrChar+1 at %d is %d\n", ptrChar + 1, *(ptrChar + 1));
-    printf("ptrChar+2 at %d is %d\n", ptrChar + 2, *(ptrChar + 2));
-    printf("ptrChar+3 at %d is %d\n", ptrChar + 3, *(ptrChar + 3));
+    printf("ptr at %p is %d\n", ptr, *ptr);
+    printf("ptrChar+0 at %p is %d\n", ptrChar + 0, *ptrChar);
+    printf("ptrChar+1 at %p is %d\n", ptrChar + 1, *(ptrChar + 1));
+    printf("ptrChar+2 at %p is %d\n", ptrChar + 2, *(ptrChar + 2));
+    printf("ptrChar+3 at %p is %d\n", ptrChar + 3, *(ptrChar + 3));
     linebreak();
 
     // ! ptr as function arguments. ! //
