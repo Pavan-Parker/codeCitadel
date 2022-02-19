b6a9c707a23d59800bafb4e7588d96d9c5703409
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-19 17:22:26 +0530

    February 19, 2022 5:22 PM

---
+++ codes/interview_practice/LL.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Node
+{
+    int value;
+    Node *next;
+};
+
+int main()
+{
+    int given[] = {1, 24, 12, 9, 24, 25, 10, 3, 2, 2};
+    printf("%d") sizeof(given);
+    return 0;
+}
