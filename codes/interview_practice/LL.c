b16bbe764ef2d4e57c001a9c745d20b2ee4ab8ee
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-19 17:47:08 +0530

    February 19, 2022 5:47 PM

--- codes/interview_practice/LL.c
+++ codes/interview_practice/LL.c
@@ -6,7 +6,7 @@ struct Node
     struct Node *next;
 };
 
-Node *createLL(int *array, int size)
+struct Node *createLL(int *array, int size)
 {
     struct Node *head;
     struct Node *temp = head;
@@ -24,12 +24,41 @@ Node *createLL(int *array, int size)
         }
         temp = temp->next;
     }
+
+    return head;
+}
+
+void destroyLL(struct Node *head)
+{
+    struct Node *temp = head;
+    struct Node *past;
+    while (temp != NULL)
+    {
+        past = temp;
+        temp = temp->next;
+        free(past);
+    }
+}
+
+void printLL(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->value);
+        temp = temp->next;
+    }
+    printf("NULL");
 }
 
 int main()
 {
+
     int given[] = {1, 24, 12, 9, 24, 25, 10, 3, 2, 2};
     printf("given:%zu bytes", sizeof(given));
 
+    struct Node *head = createLL(given, sizeof(given) / sizeof(int));
+    printLL(head);
+    destroyLL(head);
     return 0;
 }
