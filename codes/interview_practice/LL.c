139ad8fe47bf21a007336ed6d8d0dcc416a318f9
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-19 17:27:58 +0530

    February 19, 2022 5:27 PM

--- codes/interview_practice/LL.c
+++ codes/interview_practice/LL.c
@@ -3,12 +3,23 @@
 struct Node
 {
     int value;
-    Node *next;
+    Node *next=NULL;
 };
+
+Node* createLL(int *array, int size)
+{
+    Node* head;
+    Node* temp = head;
+    for(int i=0;i<size;i++)
+    {
+        temp
+    }
+}
 
 int main()
 {
     int given[] = {1, 24, 12, 9, 24, 25, 10, 3, 2, 2};
-    printf("%d") sizeof(given);
+    printf("given:%zu bytes",sizeof(given));
+    
     return 0;
 }
