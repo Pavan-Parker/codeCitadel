37f4e9b8ff2849888819e9c304df95ef9f9e410c
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-22 20:30:22 +0530

    February 22, 2022 8:30 PM

--- codes/c/reverseFirstNBits.c
+++ codes/c/reverseFirstNBits.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MASK_LSB(x, i) ((x << (i)) >> (i))
 #define MASK_MSB(x, i) ((x >> (i)) << (i))
-int reverseNBits(int x, int N)
+unsigned int reverseNBits(unsigned int x, unsigned int N)
 {
     /*
      * 0. intiate count to N.
@@ -15,11 +15,11 @@ int reverseNBits(int x, int N)
      * 8. Left shift the result by remaining count.
      * 9. Copy LSB to result and return result.
      */
-    int lsb = MASK_LSB(x, 32 - N);
-    int msb = MASK_MSB(x, N);
+    unsigned int lsb = MASK_LSB(x, 32 - N);
+    unsigned int msb = MASK_MSB(x, N);
 
-    int result = msb;
-    int count = N;
+    unsigned int result = msb;
+    unsigned int count = N;
 
     msb >>= 32 - N;
 
@@ -36,9 +36,9 @@ int reverseNBits(int x, int N)
 }
 int main()
 {
-    int given, result, N;
-    scanf("%d", &given);
-    scanf("%d", &N);
+    unsigned int given, result, N;
+    scanf("%u", &given);
+    scanf("%u", &N);
     result = reverseNBits(given, N);
     printf("Given:%x, N:%x\n", given, N);
     printf("result:%x\n", result);
