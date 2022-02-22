9cc32dd03ac002ba01f8f6252a5aca65adfa22d3
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-22 21:45:09 +0530

    February 22, 2022 9:45 PM

--- codes/c/reverseFirstNBits.c
+++ codes/c/reverseFirstNBits.c
@@ -4,6 +4,7 @@
 
 void printInBinary(unsigned int x)
 {
+    printf("%s",#x);
     for (unsigned int i = 0; i < (sizeof(x) * 8); i++)
         printf("%u", (x >> i) && 1);
     printf("\n");
@@ -22,22 +23,23 @@ unsigned int reverseNBits(unsigned int x, unsigned int N)
      * 6. Decrement count by one.
      * 7. Repeat 4 if MSB isn't zero.
      * 8. Left shift the result by remaining count.
-     * 9. Copy LSB to result and return result.
+     * 9. Copy LSB to result and return result 
      */
     unsigned int lsb = MASK_LSB(x, 32 - N);
     unsigned int msb = MASK_MSB(x, N);
-
+    printInBinary()
     unsigned int result = msb;
     unsigned int count = N;
 
     msb >>= (32 - N);
 
-    while (msb)
+    while(msb)
     {
         result |= (msb & 1);
         msb >>= 1;
         result <<= 1;
         count--;
+        printInBinary(result);
     }
     result <<= count;
     result |= lsb;
@@ -45,9 +47,9 @@ unsigned int reverseNBits(unsigned int x, unsigned int N)
 }
 int main()
 {
-    unsigned int given, result, N;
-    scanf("%u", &given);
-    scanf("%u", &N);
+    unsigned int given =1024, result, N=20;
+    // scanf("%u", &given);
+    // scanf("%u", &N);
     result = reverseNBits(given, N);
     printf("Given: ");
     printInBinary(given);
