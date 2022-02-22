61d358153735e6a46b5d98e3d96fb9cd8ab3c492
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-22 20:10:12 +0530

    February 22, 2022 8:10 PM

--- codes/c/reverseFirstNBits.c
+++ codes/c/reverseFirstNBits.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-#define MASK_LSB (x, i) ((x << (i)) >> (i))
-#define REVERSE_MSB (x, i) ((x << (i)) >> (i))
+#define MASK_LSB (x, i)((x << (i)) >> (i))
+#define MASK_MSB (x, i)((x >> (i)) << (i))
+int reverseNBits(int x, int i)
+{
+    int lsb = MASK_LSB(x, 32 - i);
+    int msb = MASK_MSB(x, i);
+    
+    int result_msb = msb;
+    int count = 
+}
 int main()
 {
     int given, result, N;
     scanf("%d", &given);
-    result = MASK_LSB(given, 32 - N) || REVERSE_MSB(given, i);
     return 0;
 }
