838dafcfc59f3b6247f0ce52371c471e8e705fd9
Pavan_Parker <ppavanrockz@gmail.com>
2022-02-22 20:17:18 +0530

    February 22, 2022 8:17 PM

--- codes/c/reverseFirstNBits.c
+++ codes/c/reverseFirstNBits.c
@@ -3,14 +3,25 @@
 #define MASK_MSB (x, i)((x >> (i)) << (i))
 int reverseNBits(int x, int N)
 {
+    /*
+     * 1. Mask and save lsb for later.
+     * 2. Mask msb for operation.
+     * 3. Shift MSB to rightmost.
+     * 4, Copy the last bit of msb to last bit of result.
+     * 5. Right shift MSB and left shift result by one.
+     * 6. 
+     */
     int lsb = MASK_LSB(x, 32 - N);
     int msb = MASK_MSB(x, N);
 
     int result_msb = msb;
     int count = sizeof(int) * 8 - 1;
-    while(msb)
+
+    msb >>= 32 - N;
+
+    while (msb)
     {
-        
+
     }
 }
 int main()
